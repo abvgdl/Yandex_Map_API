974f9f80cbb61a2829482725c6dbc7de67585dd0
abvgdl <senior.abdulmanoff@yandex.ru>
2024-03-31 16:59:23 +0500

    2.0

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-
 import sys
 import requests
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
 
 class MapWindow(QMainWindow):
     def __init__(self, latitude, longitude, zoom):
@@ -11,18 +12,38 @@ class MapWindow(QMainWindow):
         self.setWindowTitle("Yandex Map")
         self.setGeometry(100, 100, 800, 600)
 
-        api_key = "40d1649f-0493-4b70-98ba-98533de7710b"
-        map_url = f"https://static-maps.yandex.ru/1.x/?ll={longitude},{latitude}&z={zoom}&l=map&size=650,450"
+        self.latitude = latitude
+        self.longitude = longitude
+        self.zoom = zoom
 
+        self.api_key = "40d1649f-0493-4b70-98ba-98533de7710b"
+
+        self.label = QLabel(self)
+        self.label.setGeometry(0, 0, 800, 600)
+
+        self.update_map()
+
+    def update_map(self):
+        map_url = f"https://static-maps.yandex.ru/1.x/?ll={self.longitude},{self.latitude}&z={self.zoom}&l=map&size=650,450"
+        print(map_url)
         response = requests.get(map_url)
-        print(response.status_code, map_url)
 
         if response.status_code == 200:
             pixmap = QPixmap()
             pixmap.loadFromData(response.content)
-            label = QLabel(self)
-            label.setPixmap(pixmap)
-            label.resize(650, 450)
+            self.label.setPixmap(pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageDown:
+            if self.zoom > 0:
+                self.zoom -= 1
+            self.update_map()
+
+        if event.key() == Qt.Key_PageUp:
+            if self.zoom < 23:
+                self.zoom += 1
+            self.update_map()
+
 
 if __name__ == "__main__":
     latitude = 55.7558
